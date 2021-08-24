db93f4dd069cfb1bddadd57927697329eaa960f4
Carlos Franco <me@cfrco.com>
2021-08-24 14:06:53 -0700

    final commit

--- src/app.js
+++ src/app.js
@@ -33,13 +33,14 @@
             }
             }
           list.items = found;
-          if(list.items.length == 0){
-            list.message = "Nothing Found!!";
-          } else {
-            list.message = "";
-          }
+
         } else {
           list.items = response.data.menu_items;
+        }
+        if(list.items.length != 0){
+          list.message = "";
+        } else {
+          list.message = "Nothing Found!!";
         }
       })
         .catch(function (error) {
